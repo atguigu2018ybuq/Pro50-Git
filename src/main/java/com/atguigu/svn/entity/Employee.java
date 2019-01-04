3273fbb09ae82f5a06e0e6fa5a1ffbbfc6926d7e
wuyanzu <wuyanzu@atguigu.com>
2019-01-04 16:32:31 +0800

    在Eclipse中提交文件到本地库

---
+++ src/main/java/com/atguigu/svn/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.atguigu.svn.entity;
+
+public class Employee {
+    
+    public static void main(String[] args) {
+        System.out.println("hello...");
+        System.err.println("hhll...");
+        System.out.println("aaa..");
+        System.out.println("aaa..edit by left");
+        System.out.println("aaa..xifu water");
+        System.out.println("aaa..xifu water");
+        System.out.println("aaa..xifu water");
+        System.out.println("aaa..ma water");
+        System.out.println("aaa..ma water");
+        System.out.println("aaa..ma water");
+        System.out.println("aaa..black");//edit by Tortoise svn@@@
+        System.out.println("aaa..white");
+        System.out.println("aaa..yellow");
+        System.out.println("aaa..");
+        System.out.println("aaa..");
+        System.out.println("aaa..");
+        System.out.println("aaa..");
+        System.out.println("aaa..");
+    }
+
+}
